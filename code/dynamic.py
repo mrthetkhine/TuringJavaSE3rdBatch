cd236aff77b63ec0ad5195c3dbe4d73ae3b3f894
Thet Khine <mrthetkhine@gmail.com>
2021-11-27 21:20:47 +0630

    Init

---
+++ code/dynamic.py
@@ -0,0 +1,11 @@
+a = 3
+print("A is ",type(a))
+
+a = "Hello"
+print("A is ",type(a))
+
+a = "True"
+print("A is ",type(a))
+
+b = a + 4
+print("b ",b)
---
+++ src/javase3rdbatch/JavaSE3rdBatch.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javase3rdbatch;
+
+/**
+ *
+ * @author thetkhine
+ */
+public class JavaSE3rdBatch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        int a = 3;
+        System.out.println("Hello World "+a);
+        System.out.println(5/2);
+        //a = "Hello";
+    }
+    
+}
